b61a4d755e1b675fcbde64e11bb46643211a72cd
Krzysztof <krajzys@o2.pl>
2019-11-18 16:48:49 +0100

    Troche pracy nad wykrywaniem oczek najpierw, dodalem .idea do .gitignore bo uzywam pycharma

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import cv2 as cv
 import numpy as np
 import argparse
 import random as rng
+import math
+
+def distCircle(circle, circle2):
+    distx = circle[0] - circle2[0] if circle[0] > circle2[0] else circle2[0] - circle[0]
+    disty = circle[1] - circle2[1] if circle[1] > circle2[1] else circle2[1] - circle[1]
+    distance = math.sqrt(pow(distx,2) + pow(disty,2))
+    return distance
+
+
 
 source_window = 'Dices'
 grayscale = 'Grayed'
@@ -10,74 +19,140 @@ canny_window = 'Canny Applied'
 threshold_window = 'Threshold Applied'
 
 parser = argparse.ArgumentParser(description='Code for Finding contours in your image tutorial.')
-parser.add_argument('--input', help='Path to input image.')
+parser.add_argument('-i', '--input', help='Path to input image.', nargs='+')
 args = parser.parse_args()
+print(args.input)
 
-src = cv.imread(args.input)
-src = cv.resize(src, (800, 600))
+srcs = []
+for i in args.input:
+    srcs.append(cv.imread(i))
 
-if src is None:
-    print('Could not open or find the image:', args.input)
-    exit(0)
+addit = ['Photos/Easy/easy11.jpg',
+         'Photos/Easy/easy12.jpg',
+         'Photos/Easy/easy13.jpg',
+         'Photos/Easy/easy14.jpg',
+         'Photos/Easy/easy15.jpg',
+         'Photos/Easy/easy16.jpg',
+         'Photos/Easy/easy17.jpg',
+         'Photos/Easy/easy18.jpg']
 
-# Convert image to gray
-src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-src_gray = cv.medianBlur(src_gray, 5)
+for i in addit:
+    srcs.append(cv.imread(i))
 
-# Thresholding
-ret, thresh = cv.threshold(src_gray, 130, 255, cv.THRESH_BINARY)
-#thresh = cv.adaptiveThreshold(src_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 27, 1)
-canny = cv.Canny(thresh, 2, 4, 10)
-canny = cv.blur(canny, (3, 3))
+print(len(srcs))
 
-# Finding contours
-contours, hierarchy = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+#1 2 3 5 7 9 10
+for src in srcs:
+    if src is None:
+        print('Could not open or find the image:', args.input)
+        exit(0)
 
-# Getting bounding rectangles and circles
-contours_poly = [None]*len(contours)
-boundRect = [None]*len(contours)
-centers = [None]*len(contours)
-radius = [None]*len(contours)
-for i, c in enumerate(contours):
-    contours_poly[i] = cv.approxPolyDP(c, 3, True)
-    boundRect[i] = cv.boundingRect(contours_poly[i])
-    centers[i], radius[i] = cv.minEnclosingCircle(contours_poly[i])
+    # Convert image to gray
+    src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    src_gray = cv.blur(src_gray, (3,3))
+    src_gray = cv.medianBlur(src_gray, 21)
 
-dices = []
+    # Thresholding
+    _, thresh = cv.threshold(src_gray, 40, 255, cv.THRESH_BINARY)
+    print(len(thresh), len(thresh[0]))
 
-# Drawing contorus and bounding rectangles
-for i in range(len(contours)):
-    color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-    cv.drawContours(src, contours_poly, i, color)
-    if (cv.contourArea(contours[i]) > 700):
-        dices.append(src_gray[boundRect[i][1] : boundRect[i][1] + boundRect[i][3], boundRect[i][0] : boundRect[i][0] + boundRect[i][2]])
-        cv.rectangle(src, (int(boundRect[i][0]), int(boundRect[i][1])), \
-              (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
+    # Fill corners of image with white
+    cv.floodFill(thresh, np.zeros((len(thresh)+2, len(thresh[0])+2), np.uint8), (0, 0), 255)
+    cv.floodFill(thresh, np.zeros((len(thresh)+2, len(thresh[0])+2), np.uint8), (len(thresh[0])-1, len(thresh)-1), 255)
+    cv.floodFill(thresh, np.zeros((len(thresh)+2, len(thresh[0])+2), np.uint8), (0, len(thresh) - 1), 255)
+    cv.floodFill(thresh, np.zeros((len(thresh)+2, len(thresh[0])+2), np.uint8), (len(thresh[0])-1, 0), 255)
 
-det = cv.SimpleBlobDetector().create()
-k = 1
-for i in dices:
-    i = cv.adaptiveThreshold(i, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 17, 1)
-    cv.imshow(str(k), i)
-    print(k, len(det.detect(i)))
-    k += 1
+    # Get canny for edges
+    canny = cv.Canny(thresh, 2, 4)
+    canny = cv.blur(canny, (3, 3))
+
+    # Finding contours
+    contours, hierarchy = cv.findContours(canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    # Finding circles
+    circles = cv.HoughCircles(src_gray, cv.HOUGH_GRADIENT, 2, 100, param1 = 35, param2 = 30, minRadius = 30, maxRadius = 70)
+
+    circles = np.uint16(np.around(circles))
+    betterCircles = []
+
+    # Check to find right circles and append them to betterCircles
+    index_k = 0
+    for i in circles[0, :]:
+        my_x = i[0] - i[2]
+        my_y = i[1] - i[2]
+        rect = cv.rectangle(src, (my_x, my_y), (my_x + i[2]*2, my_y + i[2]*2), (255, 0, 0), 4)
+        cv.circle(src, (i[0], i[1]), i[2], (0, 0, 255), 2)
+        if np.mean(thresh[my_y : my_y + i[2]*2, my_x : my_x + i[2]*2]) < 100:
+            cv.putText(src, "Kropka" + str(index_k), (my_x, my_y), cv.FONT_HERSHEY_PLAIN, 4, (255, 255, 0), 10)
+            betterCircles.append(i)
+            index_k += 1
+            cv.circle(src, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv.circle(src, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+    # TODO Poprawić to tutaj
+    # pozwolić na konflikty i je rozwiązywać
+    # poprzez najmnijesza srednia odleglosc od oczka
+
+    ungrouped = betterCircles[:]
+    grouped = []
+    groups = []
+    
+    # Grouping of dots
+    for ind in range(len(ungrouped)):
+        circle = ungrouped[ind]
+        if ind in grouped:
+            continue
+        grouped.append(ind)
+        group = [ind]
+        groups.append(group)
+        radius = int(circle[2]*7.4)
+        for i in range(len(ungrouped)):
+            if ind != i and i not in grouped: # circle is not the same and isnt yet grouped
+                circle2 = ungrouped[i]
+                #print("Indeksy", ind, i)
+                #print("X Y", circle[0], circle[1], circle2[0], circle2[1])
+                if distCircle(circle, circle2) <= radius:
+                    grouped.append(i)
+                    groups[-1].append(i)
+                    #group.append(i)
+    
 
 
-# Create Windows
-cv.namedWindow(source_window, cv.WINDOW_NORMAL)
-cv.resizeWindow(source_window, 800, 600)
-cv.imshow(source_window, src)
+    print(groups)
+    # Draw rectangles around dices
+    for i in groups:
+        minx = 10000
+        maxx = -1
+        miny = 10000
+        maxy = -1
+        for j in i:
+            if betterCircles[j][0] < minx:
+                minx = betterCircles[j][0] - betterCircles[j][2]
+            if betterCircles[j][0] > maxx:
+                maxx = betterCircles[j][0] + betterCircles[j][2]
+            if betterCircles[j][1] < miny:
+                miny = betterCircles[j][1] - betterCircles[j][2]
+            if betterCircles[j][1] > maxy:
+                maxy = betterCircles[j][1] + betterCircles[j][2]
+        cv.rectangle(src, (minx, miny), (maxx, maxy), (255, 128, 0), 10)
+        cv.putText(src, "Kostka: " + str(len(i)), (minx, miny), cv.FONT_HERSHEY_PLAIN, 4, (255, 255, 0), 10)
 
-cv.namedWindow(grayscale, cv.WINDOW_NORMAL)
-cv.resizeWindow(grayscale, 800, 600)
-cv.imshow(grayscale, src_gray)
 
-cv.namedWindow(threshold_window, cv.WINDOW_NORMAL)
-cv.resizeWindow(threshold_window, 800, 600)
-cv.imshow(threshold_window, thresh)
+    # Create Windows
+    cv.namedWindow(source_window, cv.WINDOW_NORMAL)
+    cv.resizeWindow(source_window, 800, 600)
+    cv.imshow(source_window, src)
 
-cv.namedWindow(canny_window, cv.WINDOW_NORMAL)
-cv.resizeWindow(canny_window, 800, 600)
-cv.imshow(canny_window, canny)
+    cv.namedWindow(grayscale, cv.WINDOW_NORMAL)
+    cv.resizeWindow(grayscale, 800, 600)
+    cv.imshow(grayscale, src_gray)
 
-cv.waitKey()
+    cv.namedWindow(threshold_window, cv.WINDOW_NORMAL)
+    cv.resizeWindow(threshold_window, 800, 600)
+    cv.imshow(threshold_window, thresh)
+
+    cv.namedWindow(canny_window, cv.WINDOW_NORMAL)
+    cv.resizeWindow(canny_window, 800, 600)
+    cv.imshow(canny_window, canny)
+
+    cv.waitKey()
